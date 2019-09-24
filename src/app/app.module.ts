5b66da2d3ccae17bd9b25acee20eb640a9506192
Ubiratan Leitão Mourão <ubiratan.mourao@gmail.com>
2019-09-23 22:17:12 -0300

    Insersão de teste da pagina de cadastro de usuário

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsuarioComponent } from './usuario/usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuarioComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/usuario/usuario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cadastro-usuario',
+  templateUrl: './usuario.component.html',
+  styleUrls: ['./usuario.component.css']
+})
+export class UsuarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
