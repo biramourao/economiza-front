adb481874d38ceabe8eea9d8be0de6bf953be299
Ubiratan Leitão Mourão <ubiratan.mourao@gmail.com>
2019-11-14 15:32:50 -0300

    Acrecimo dos metodos de listFontesDeRenda, pagarGasto

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -2,16 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthLoginInfo } from './auth/auth-login-info';
 import { SignUpInfo } from './auth/sign-up-info';
-import { Observable } from 'rxjs';
 import { CategoriaGasto } from './model/categoria-gasto';
 import { Gasto } from './model/gasto';
+import { FonteDeRenda } from './model/fonte-de-renda';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-apiURL = 'http://localhost:8080';
+  apiURL = 'http://localhost:8080';
 
   constructor(private http: HttpClient) { }
 
@@ -19,7 +19,7 @@ apiURL = 'http://localhost:8080';
     const url = `${this.apiURL}/usuarios/login`;
     return this.http.post<AuthLoginInfo>(url, usuario)
   }
-    cadastraUsuario(usuario: any) {
+  cadastraUsuario(usuario: any) {
     const url = `${this.apiURL}/usuarios`;
     return this.http.post<SignUpInfo>(url, usuario);
   }
@@ -54,5 +54,13 @@ apiURL = 'http://localhost:8080';
   excluirGasto(cod: any) {
     const url = `${this.apiURL}/gastos/${cod}`;
     return this.http.delete<any>(url);
+  }
+  listFontesDeRenda() {
+    const url = `${this.apiURL}/fontes-de-renda`;
+    return this.http.get<FonteDeRenda>(url, { responseType: 'json' });
+  }
+  pagarGasto(cod: any) {
+    const url = `${this.apiURL}/gastos/${cod}/pagamento`;
+    return this.http.patch<Gasto>(url, { responseType: 'json' });
   }
 }
