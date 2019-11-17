93d6990d561ebe6014738904f499c012eb801757
jhonvg13 <55266953+jhonvg13@users.noreply.github.com>
2019-11-17 04:24:08 -0300

    Criação dos metodos da fonte de renda no apiService

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -92,6 +92,21 @@ export class ApiService {
   listFontesDeRenda() {
     const url = `${this.apiURL}/fontes-de-renda`;
     return this.http.get<FonteDeRenda>(url, { responseType: 'json' });
-
+  }
+  cadastrarFonteDeRenda(fonteDeRenda: FonteDeRenda){
+    const url = `${this.apiURL}/fontes-de-renda`;
+    return this.http.post<FonteDeRenda>(url, fonteDeRenda);
+  }
+  editarFonteDeRenda(fonteDeRenda: FonteDeRenda){
+    const url = `${this.apiURL}/fontes-de-renda/${fonteDeRenda.cod}`;
+    return this.http.put<FonteDeRenda>(url, fonteDeRenda);
+  }
+  excluirFonteDeRenda(cod: any){
+    const url = `${this.apiURL}/fontes-de-renda/${cod}`;
+    return this.http.delete<any>(url);
+  }
+  getFonteDeRenda(cod: any) {
+    const url = `${this.apiURL}/fontes-de-renda/${cod}`;
+    return this.http.get<FonteDeRenda>(url);
   }
 }
